a8474d7d1b677cc780fe007eff27bbc8d1fdf48c
mohanball0427 <19215A0506@bvrit.ac.in>
2022-09-12 16:00:17 +0530

    first commit

---
+++ src/main/java/com/lti/Project_jenkins_Git_Demo/App.java
@@ -0,0 +1,13 @@
+package com.lti.Project_jenkins_Git_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
